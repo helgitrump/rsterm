c6907c398c6a6900e7a7c91afc7d45f948afaf0f
Oleg Kozyr <olegkozyr86@gmail.com>
2017-07-10 09:52:41 +0300

    Add filds and its initialization to MainForm. Filds control serial port parameters

--- rsterm/MainForm.cs
+++ rsterm/MainForm.cs
@@ -17,6 +17,17 @@ namespace rsterm
 	/// </summary>
 	public partial class MainForm : Form
 	{
+        #region Filds
+        private SerialPort sPort;
+        private const string _RS_Init_PortName = "COM1";
+        private const int _RS_Init_BaudRate = 9600;
+        private const Parity _RS_Init_Parity = Parity.None;
+        private const int _RS_Init_DataBits = 8;
+        private const StopBits _RS_Init_StopBits = StopBits.One;
+        private const bool _RS_Init_RST_Enable = false;
+        private const bool _RS_Init_DSR_Enable = false;
+        #endregion Filds		
+		
 		public MainForm()
 		{
 			//
