d7792b579abf217fce754867fd46c823e8d09dd4
Oleg Kozyr <olegkozyr86@gmail.com>
2017-07-10 10:06:44 +0300

    Add function to initialize rsPort field

--- rsterm/MainForm.cs
+++ rsterm/MainForm.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace rsterm
 {
@@ -18,7 +19,7 @@ namespace rsterm
 	public partial class MainForm : Form
 	{
         #region Filds
-        private SerialPort sPort;
+        private SerialPort rsPort;
         private const string _RS_Init_PortName = "COM1";
         private const int _RS_Init_BaudRate = 9600;
         private const Parity _RS_Init_Parity = Parity.None;
@@ -39,5 +40,20 @@ namespace rsterm
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		
+		private void InitRS_Port()
+        {// Initialize rsPort field
+            rsPort = new SerialPort(
+                _RS_Init_PortName, 
+                _RS_Init_BaudRate,
+                _RS_Init_Parity,
+                _RS_Init_DataBits,
+                _RS_Init_StopBits);
+            rsPort.RtsEnable = _RS_Init_RST_Enable;
+            rsPort.DtrEnable = _RS_Init_DSR_Enable;
+            rsPort.ReadTimeout = 2000;
+            rsPort.WriteTimeout = 2000;
+        }
+		
 	}
 }
